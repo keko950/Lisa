556471a88b1dc7b3f6799ac62c80207b36078b79
gilberto.plaza <keko950@hotmail.com>
2019-08-14 00:38:42 +0200

    Added KeyPressed and KeyReleased events

--- Lisa/src/Lisa/Events/KeyEvent.h
+++ Lisa/src/Lisa/Events/KeyEvent.h
@@ -3,6 +3,8 @@
 #include "Event.h"
 #include "../Core.h"
 
+#include <sstream>
+
 namespace Lisa
 {
 	class LISA_API KeyEvent : public Event 
@@ -21,6 +23,37 @@ namespace Lisa
 	class LISA_API KeyPressedEvent : public KeyEvent
 	{
 	public:
+		KeyPressedEvent(int keycode, int repeatCount) : KeyEvent(keycode), m_RepeatCount(repeatCount) {}
+		
 		EVENT_CLASS_TYPE(KeyPressed)
+
+		inline int GetRepeatCount() const { return m_RepeatCount;  }
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyPressedEvent: " << m_KeyCode << "( repeated: " << m_RepeatCount << " )";
+			return ss.str();
+		}
+
+	private:
+		int m_RepeatCount;
+
+	};
+
+	class LISA_API KeyReleasedEvent : public KeyEvent
+	{
+	public:
+		KeyReleasedEvent(int keycode) : KeyEvent(keycode) {};
+		
+		EVENT_CLASS_TYPE(KeyReleased);
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyReleasedEvent: " << m_KeyCode;
+			return ss.str();
+		}
+
 	};
 }
