171a8485b1906c97857924d8aac2f88cc3108554
gilberto.plaza <keko950@hotmail.com>
2019-08-17 23:30:54 +0200

    Added EventDispatcher

--- Lisa/src/Lisa/Events/ApplicationEvent.h
+++ Lisa/src/Lisa/Events/ApplicationEvent.h
@@ -89,4 +89,31 @@ namespace Lisa
 		EVENT_CLASS_TYPE(WindowMoved)
 		EVENT_CLASS_CATEGORY(EventCategoryApplication)
 	};
+
+	class LISA_API AppTickEvent : public Event
+	{
+	public:
+		AppTickEvent() {}
+
+		EVENT_CLASS_TYPE(AppTick)
+			EVENT_CLASS_CATEGORY(EventCategoryApplication)
+	};
+
+	class LISA_API AppUpdateEvent : public Event
+	{
+	public:
+		AppUpdateEvent() {}
+
+		EVENT_CLASS_TYPE(AppUpdate)
+			EVENT_CLASS_CATEGORY(EventCategoryApplication)
+	};
+
+	class LISA_API AppRenderEvent : public Event
+	{
+	public:
+		AppRenderEvent() {}
+
+		EVENT_CLASS_TYPE(AppRender)
+			EVENT_CLASS_CATEGORY(EventCategoryApplication)
+	};
 }
--- Lisa/src/Lisa/Events/Event.h
+++ Lisa/src/Lisa/Events/Event.h
@@ -56,8 +56,32 @@ namespace Lisa
 
 	class EventDispatcher
 	{
+		template<typename T>
+		using EventFn = std::is_function<bool(T&)>;
 
+	public:
+		EventDispatcher(Event& event)
+			: m_Event(event) {};
+
+		template<typename T>
+		bool Dispatch(EventFn<T> func)
+		{
+			if (m_Event.GetEventType() == T::GetStaticType())
+			{
+				m_Event.m_handled = func(*(T*)&m_Event);
+				return true;
+			}
+			return false;
+		}
+
+	private:
+		Event& m_Event;
 	};
+
+	inline std::ostream& operator<<(std::ostream& os, const Event& e)
+	{
+		return os << e.ToString();
+	}
 
 }
 
