e6f552f2a1f9c909829b52bb682c753847324786
gilberto.plaza <keko950@hotmail.com>
2019-09-07 02:00:53 +0200

    Abstracted Shader

---
+++ Lisa/src/Lisa/Renderer/Shader.cpp
@@ -0,0 +1,22 @@
+#include "lspch.h"
+
+#include "Lisa\Core.h"
+#include "Shader.h"
+#include "Renderer.h"
+
+#include "Platform\OpenGL\OpenGLShader.h"
+namespace Lisa
+{
+	Shader* Shader::Create(const std::string vertexSrc, const std::string fragmentSrc)
+	{
+		switch (Renderer::GetAPI())
+		{
+			case RendererAPI::OpenGL:
+				return new OpenGLShader(vertexSrc, fragmentSrc);
+				break;
+			default:
+				return nullptr;
+				break;
+		}
+	}
+}
--- Lisa/src/Lisa/Renderer/Shader.h
+++ Lisa/src/Lisa/Renderer/Shader.h
@@ -9,12 +9,13 @@ namespace Lisa
 	class LISA_API Shader
 	{
 	public:
-		Shader() {};
 		~Shader() = default;
 
 		// use/activate the shader
 		virtual void Bind() const {};
 		virtual void UnBind() const {};
+
+		static Shader* Create(const std::string vertexSrc, const std::string fragmentSrc);
 
 	protected:
 		uint32_t m_RenderId;
